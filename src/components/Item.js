c61c2c8fd1209ebfbc21b13d63d51162e6820182
Nihar Patel <niharpatel52@gmail.com>
2025-06-12 11:15:51 -0400

    Add Add to cart toggle

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  const handleCart = () => setInCart(!inCart);
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "in-Cart" : ""} onClick={handleCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
