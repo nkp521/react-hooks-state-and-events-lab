31d58898088abb0fccdc2d6fae5ec9cf2f508881
Nihar Patel <niharpatel52@gmail.com>
2025-06-12 10:19:48 -0400

    Add dark/light mode toggle

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  const [colorMode, setColorMode] = useState(false);
+  const appClass = colorMode ? "App dark" : "App light";
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const handleColorMode = () => {
+    setColorMode((colorMode) => !colorMode);
+  };
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleColorMode}>
+          {colorMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
